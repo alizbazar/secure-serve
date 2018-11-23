4d2ffebf3d26ae9ba65e08f59732ad7bca258df1
Albert Nazander <albert@meruhealth.com>
2018-11-23 12:37:04 -0800

    Add request logger

--- serve.js
+++ serve.js
@@ -23,23 +23,28 @@ const cli = meow(`
   },
 })
 
+function exitWithErrors (...errors) {
+  errors.forEach(err => {
+    console.error(err)
+  })
+  process.exit(1)
+}
+
 const authorizationKey = uuid()
 const [filename] = cli.input
 const port = cli.flags.port || 5002
 
 if (!filename) {
-  console.log('Error: <targetFile> was empty.\n\nLaunch with node serve.js <targetFile>')
-  process.exit()
+  exitWithErrors('Error: <targetFile> was empty.\n\nLaunch with node serve.js <targetFile>')
 }
 
 const fileContent = fs.readFileSync(path.resolve(filename), 'utf8')
 if (!fileContent) {
-  console.log('File content was empty or file did not exist')
-  process.exit()
+  exitWithErrors('File content was empty or file did not exist')
 }
 
 const app = express()
-
+app.use(loggerMiddleware)
 app.get('/get', authorize, getFile)
 
 const server = app.listen(port, startTunnel)
@@ -53,14 +58,33 @@ process.on('SIGTERM', () => {
 function startTunnel () {
   localtunnel(port, (err, tunnel) => {
     if (err) {
-      console.log('Tunnel could not be established')
-      console.log(err)
-      process.exit()
+      exitWithErrors('Tunnel could not be established', err)
     }
 
     console.log('\nServing file successfully. To download elsewhere, run:\n')
-    console.log(`\tcurl -H "Authorization: Bearer ${authorizationKey}" ${tunnel.url}/get > "${path.basename(filename)}"`)
+    console.log(`\tcurl -H "Authorization: Bearer ${authorizationKey}" ${tunnel.url}/get > "${path.basename(filename)}"\n`)
   })
+}
+
+function loggerMiddleware (req, res, next) {
+  const t = process.hrtime()
+  const logRequest = (code = '?') => {
+    const time = process.hrtime(t)
+    const timeInMS = (time[0] * 1000 + time[1] / 1000000).toFixed(0)
+    console.log(`${req.method} ${req.originalUrl} -> ${code} (${timeInMS}ms)`)
+  }
+  // Make sure requests lasting too long are logged
+  const timeout = setTimeout(logRequest, 2000)
+
+  const loggerOnFinish = () => {
+    clearTimeout(timeout)
+    res.removeListener('finish', loggerOnFinish)
+    logRequest(res.statusCode)
+  }
+
+  res.on('finish', loggerOnFinish)
+
+  next()
 }
 
 const authorizationPattern = /^Bearer (.+)$/i
